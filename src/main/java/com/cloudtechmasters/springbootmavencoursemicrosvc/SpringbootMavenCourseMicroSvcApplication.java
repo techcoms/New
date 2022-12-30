90e6771d7d154b262a05f446ef11cc14f9327784
sahadeva <sahadeva9478@gmail.com>
2022-12-31 00:56:23 +0530

    Initial commit

---
+++ src/main/java/com/cloudtechmasters/springbootmavencoursemicrosvc/SpringbootMavenCourseMicroSvcApplication.java
@@ -0,0 +1,13 @@
+package com.cloudtechmasters.springbootmavencoursemicrosvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMavenCourseMicroSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMavenCourseMicroSvcApplication.class, args);
+	}
+
+}
